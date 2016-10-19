ba92da2745953ed3bbc2a794ff679da487c586ad
Erica Nafziger and Luke Philips <erica@>
2016-10-19 09:33:03 -0700

    add initial files, some css to style, and jQuery functionality

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#initialForm form").submit(function(){
+    var nameOfBossInput = $(input#nameOfBoss).var()
+    var lengthOfNoticeInput = $(input#lengthOfNotice).var()
+    var nameOfCompanyInput = $(input#nameOfCompany).var()
+    var lengthOfEmploymentInput = $(input#lengthOfEmployment).var()
+    var careerInput = $(input#career).var()
+    var newCareerInput = $(input#newCareer).var()
+    var yourNameInput = $(input#yourName).var()
+
+    $("")
+  });
+});
