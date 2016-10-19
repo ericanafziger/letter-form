1c95d10fda15ad5d120bcd81bbdf09d4c900208b
Erica Nafziger and Luke Philips <luke@>
2016-10-19 10:17:49 -0700

    add headings and more css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
+  var today = new Date();
+  var day = today.getDate();
+  var month = today.getMonth() +1;
+  var year = today.getFullYear();
+  today = month+'/'+day+'/'+year;
+
   $("#initialForm form").submit(function(event){
     var nameOfBossInput = $("input#nameOfBoss").val();
     var lengthOfNoticeInput = $("input#lengthOfNotice").val();
     var nameOfCompanyInput = $("input#nameOfCompany").val();
+    var companyAddressInput = $("input#companyAddress").val();
+    var companyCityInput = $("input#companyCity").val();
     var lengthOfEmploymentInput = $("input#lengthOfEmployment").val();
     var careerInput = $("input#career").val();
     var newCareerInput = $("input#newCareer").val();
@@ -11,13 +19,17 @@ $(document).ready(function(){
     $(".nameOfBoss").text(nameOfBossInput);
     $(".lengthOfNotice").text(lengthOfNoticeInput);
     $(".nameOfCompany").text(nameOfCompanyInput);
+    $(".companyAddress").text(companyAddressInput);
+    $(".companyCity").text(companyCityInput);
     $(".lengthOfEmployment").text(lengthOfEmploymentInput);
     $(".career").text(careerInput);
     $(".newCareer").text(newCareerInput);
     $(".yourName").text(yourNameInput);
+    $(".todaysDate").text(today);
 
     $("#lettertext").show();
     event.preventDefault();
 
   });
+
 });
